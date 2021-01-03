e97aade805f0a481284518cfe9f03420d056892c
MichaelRosado1 <mrosad4@illinois.edu>
2021-01-02 23:07:29 -0600

    'fixed error that caused a type error within the fetch pokemon function'

--- app.js
+++ app.js
@@ -2,25 +2,27 @@ const submitButton = document.querySelector('#submit-button');
 
 submitButton.addEventListener('click', () => {
     let chosenPokemon = document.getElementById('pokemon-chosen').value;
-    let data = fetchPokemon(chosenPokemon);
-    displayData(data);
+    fetchPokemon(chosenPokemon);
 });
 
-const displayData = (data) => {
-    console.log(data);
+const displayData = async (data) => {
+    const name = await data.species.name;
+    console.log(name);
+
+
 }
 
 const fetchPokemon = async (pokemon) => {
-   if (pokemon.length >0) {
+   if (pokemon.length > 0) {
         try {
             const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon);
             let data = await response.json();
-            return data;
-            
+            new Promise(resolve => {
+                displayData(data);
+            });       
         } catch (err) {
             console.log('an error has occured:', err);
         }
     } 
 
-    console.log('input length is too short');
 }
